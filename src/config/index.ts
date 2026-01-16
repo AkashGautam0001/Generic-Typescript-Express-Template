65d82b00f06548c1a56fd53c290bd42e3c0a54c2
Akash Gautam <akashgautam01584@gmail.com>
2026-01-16 22:27:21 +0530

    completed the generic template

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,13 +5,6 @@ type ServerConfig = {
   PORT: number;
 };
 
-type DbConfig = {
-  DB_HOST: string;
-  DB_USER: string;
-  DB_PASSWORD: string;
-  DB_NAME: string;
-};
-
 export function loadEnv() {
   dotenv.config();
   logger.info("Environment variables loaded");
@@ -21,11 +14,4 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 3001,
-};
-
-export const dbConfig: DbConfig = {
-  DB_HOST: process.env.DB_HOST || "localhost",
-  DB_USER: process.env.DB_USER || "root",
-  DB_PASSWORD: process.env.DB_PASSWORD || "",
-  DB_NAME: process.env.DB_NAME || "database_development",
 };
